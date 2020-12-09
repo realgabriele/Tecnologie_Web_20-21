c737a71a8299a57091af3f41cc5b53c83bc92293
realgabriele <realgabriele@gmail.com>
2020-12-09 11:53:46 +0100

    login page

---
+++ include/classes/LoginPage.php
@@ -0,0 +1,43 @@
+<?php
+
+require "FramePublic.php";
+
+class LoginPage extends FramePublic
+{
+    private $error_msg;
+
+    public function __construct()
+    {
+        $this->body = new Template("login.html");
+        parent::__construct();
+    }
+
+    public function check_authorization($actions = [])
+    {
+        // se è gia autenticato, ridireziona verso un altra pagina
+        if($this->auth->isAuthenticated){
+            // ToDo: last page referer
+            header("Location: index.php");
+            die();
+        }
+    }
+
+    public function handleRequest()
+    {
+        if ( isset($_POST['email']) && isset($_POST['password']) ) {
+            $res = $this->auth->login($_POST['email'], $_POST['password']);
+            if (!$res['error']) {
+                // ToDo: last page referer
+                header("Location: index.php");
+                die();
+            } else {
+
+            }
+        }
+    }
+
+    public function updateBody()
+    {
+        $this->body->setContent("error_msg", $this->error_msg);
+    }
+}
---
+++ login.php
@@ -0,0 +1,9 @@
+<?php
+
+/**
+ * if not logged in:
+ * if parameters: do login
+ * in case of error: show login page with error
+ * else redirect
+ * show login page
+ */
