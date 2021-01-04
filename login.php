8b69f1fe1a0d17ed7513818d9a9c04a3507e61e6
GioInn <49196956+GioInn@users.noreply.github.com>
2021-01-04 18:00:11 +0100

    mini commit
    login

--- login.php
+++ login.php
@@ -1,6 +1,37 @@
 <?php
 
-require "include/classes/LoginPage.php";
+require "include/config.inc.php";
+require "include/template2.inc.php";
+require "include/dbms.inc.php";
 
-$page = new LoginPage();
-echo $page->getPage();
+$main = new Template("frame-public.html");
+$body = new Template("articoli/login.html");
+
+$id = $_GET["id"];
+
+if (!$result = $mysqli->query("SELECT * FROM articoli WHERE id=$id")) {
+    echo "Error: ", $mysqli->error;
+}
+
+for($i=0; $i<$result->num_rows; $i++) {
+    $data = $result->fetch_assoc();
+    $body->setContent($data, null);
+}
+
+
+$recensioni = new Template("articoli/recensioni.html");
+
+if (!$result = $mysqli->query("SELECT * FROM recensioni WHERE articolo_id=$id")) {
+    echo "Error: ", $mysqli->error;
+}
+for($i=0; $i<$result->num_rows; $i++) {
+    $data = $result->fetch_assoc();
+    $recensioni->setContent($data, null);
+}
+
+
+$body->setContent("recensioni", $recensioni->get());
+$main->setContent("body", $body->get());
+$main->close();
+
+?>
