79a2e0090755b324ba7524b1bc976311a05df617
GioInn <49196956+GioInn@users.noreply.github.com>
2021-01-27 17:55:51 +0100

    Fix Multiple Indirizzi but not  multiple Pagamenti
    Possiamo Togliere metodi pagamento e lasciarlo modificare dalla pagina dopo

--- areautente.php
+++ areautente.php
@@ -13,44 +13,56 @@ $body = new Template("areautente.html");
 $id = $_SESSION['auth_uid'];
 
 $query = "SELECT utenti.nome as nome, utenti.email as email, indirizzi.via, indirizzi.citta, indirizzi.provincia, indirizzi.cap, intestatario_carta, numero_carta, scadenza_carta 
- FROM utenti INNER JOIN indirizzi ON indirizzi.utente_id = utenti.id INNER JOIN metodipagamento ON utenti.id = metodipagamento.utente_id WHERE utenti.id=$id LIMIT 1";
+FROM utenti, indirizzi, metodipagamento WHERE indirizzi.utente_id = utenti.id AND utenti.id = metodipagamento.utente_id  GROUP BY indirizzi.via";
+
 global $dbh;
 
-if($res = $dbh->query($query)){
-    $content = $res->fetch(PDO::FETCH_ASSOC);
+    //"SELECT utenti.nome as nome, utenti.email as email, indirizzi.via, indirizzi.citta, indirizzi.provincia, indirizzi.cap, intestatario_carta, numero_carta, scadenza_carta
+ //FROM utenti INNER JOIN indirizzi ON indirizzi.utente_id = utenti.id INNER JOIN metodipagamento ON utenti.id = metodipagamento.utente_id WHERE utenti.id=$id";
 
-    if(isset($content['nome']))
-        $nome = $content['nome'];
 
-    if(isset($content['email']))
-        $email = $content['email'];
 
-    if(isset($content['via']))
-        $body->setContent('via', $content['via']);
 
-    if(isset($content['citta']))
-        $body->setContent('citta', $content['citta']);
+    if ($res = $dbh->query($query)) {
+        for($i=0; $i<$res->rowCount(); $i++) {
+            $content = $res->fetch(PDO::FETCH_ASSOC);
 
-    if(isset($content['cap']))
-        $body->setContent('cap', $content['cap']);
+            if (isset($content['nome']))
+                $nome = $content['nome'];
 
-    if(isset($content['intestatario_carta']))
-        $body->setContent('intestatario_carta', $content['intestatario_carta']);
+            if (isset($content['email']))
+                $email = $content['email'];
 
-    if(isset($content['numero_carta']))
-        $body->setContent('numero_carta', $content['numero_carta']);
+            if (isset($content['via']))
+                $body->setContent('via', $content['via']);
 
-    if(isset($content['scadenza_carta']))
-        $body->setContent('scadenza_carta', $content['scadenza_carta']);
+            if (isset($content['citta']))
+                $body->setContent('citta', $content['citta']);
 
-    if(isset($nome))
-        $body->setContent("nome", $nome);
+            if (isset($content['cap']))
+                $body->setContent('cap', $content['cap']);
 
-    if(isset($email))
-        $body->setContent("email", $email);
-} else {
-    print_r($dbh->errorInfo());
-}
+            if (isset($content['intestatario_carta']))
+                $body->setContent('intestatario_carta', $content['intestatario_carta']);
+
+            if (isset($content['numero_carta']))
+                $body->setContent('numero_carta', $content['numero_carta']);
+
+            if (isset($content['scadenza_carta']))
+                $body->setContent('scadenza_carta', $content['scadenza_carta']);
+
+            if (isset($nome))
+                $body->setContent("nome", $nome);
+
+            if (isset($email))
+                $body->setContent("email", $email);
+            else {
+                print_r($dbh->errorInfo());
+
+            }
+        }
+    }
+
 
 
 $main->setContent("body", $body->get());
