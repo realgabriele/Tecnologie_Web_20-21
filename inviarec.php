5927765e882b77f80663be81e6bfb14f1b2b656d
GioInn <49196956+GioInn@users.noreply.github.com>
2021-01-28 19:47:53 +0100

    fix $bdh

--- inviarec.php
+++ inviarec.php
@@ -7,21 +7,15 @@ $main = new Template("frame-public.html");
 $body = new Template("articoli/show_single.html");
 session_start();
 
+global $dbh;
 
-$link = mysqli_connect("localhost", "root", "", "tdw2021");
-
-
-// Connessione al db
-if($link === false){
-    die("ERROR: Could not connect. " . mysqli_connect_error());
-}
 
 // Riprendo Campi HTML
 $utente_id=$_SESSION['auth_uid'];
 $articolo_id = $_GET['id'];
-$titolo = mysqli_real_escape_string($link, $_REQUEST['titolo']);
-$descrizione = mysqli_real_escape_string($link, $_REQUEST['descrizione']);
-$rating = mysqli_real_escape_string($link, $_REQUEST['rating']);
+$titolo=($_POST['titolo']);
+$descrizione = ($_POST['descrizione']);
+$rating = ($_POST['rating']);
 
 // QUERY
 $sql = "INSERT INTO recensioni (utente_id, articolo_id, titolo,descrizione,rating) 
@@ -30,17 +24,16 @@ VALUES ('$utente_id', '$articolo_id','$titolo','$descrizione','$rating')";
 $sql_update = "UPDATE recensioni SET titolo='$titolo', descrizione='$descrizione', rating=$rating
  WHERE articolo_id=$articolo_id AND utente_id=$utente_id";
 
-if(mysqli_query($link, $sql)) {
+
+
+if($sql2=$dbh->query($sql)) {
     echo "RECENSIONE AGGIUNTA";
     header('Location: show_item.php?id=' .  $articolo_id . '&created=Recensione inserita con successo' );
-}
-
-else {
-    if(mysqli_query($link, $sql_update)) {
+} else {
+    if($sql_update2=$dbh->query($sql_update)) {
         header ('Location: show_item.php?id=' . $articolo_id . '&created=Recensione aggiornata con successo');
     } else {
         echo "ERROR: HAI GIA' INSERITO LA QUERY $sql. " . mysqli_error($link);
         header('Location: show_item.php?id=' . $articolo_id . '&rejected=Recensione già esistente');
     }
-
 }
